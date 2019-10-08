3264d338be29309294fa79387ed9fba32a1e06da
SergeiGray <borodat_for@mail.ru>
2019-10-08 20:01:06 +0300

    Task ex5_js-functions completed

---
+++ src/ex5_js-functions/task.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Calculator = (function Calculator() {
+	let result = 0;
+	function add(num = 0) {
+		result += num;
+		return add;
+	}
+	function subtract(num = 0) {
+		result -= num;
+		return subtract;
+	}
+	function divide(num = 1) {
+		if(result === 0) {
+			return divide;
+		}	
+		if(num === 0) {
+			return divide;
+		}
+		result /= num;
+		return divide;
+	}
+	function multiply(num = 0) {
+		if(result === 0) {
+			result = 1;
+		}
+		result *= num;
+		return multiply;
+	}
+	function getResult() {
+		return result
+	}
+	function reset() {
+		result = 0;
+		return result;
+	}	
+	return {add, subtract, divide, multiply, getResult, reset}
+}());
+
+module.exports = Calculator;
+
+// console.log(Calculator.divide(2)(2)(3));
+// console.log(Calculator.reset());
+
+// console.log( Calculator.getResult() );
