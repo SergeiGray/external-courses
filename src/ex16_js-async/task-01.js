9162e12f3d8261e4e8fe86beaea66316acd1683a
SergeiGray <borodat_for@mail.ru>
2019-12-02 00:46:46 +0300

    Added ability to handle errors

--- src/ex16_js-async/task-01.js
+++ src/ex16_js-async/task-01.js
@@ -1,16 +1,21 @@
 'use strict';
 
 const fulfillAJAXRequest = function (url, methodRequest = 'GET', bodyRequest = null) {
-	let xhr = new XMLHttpRequest();
-	xhr.open(methodRequest, url);
-  xhr.send(bodyRequest);
-	return new Promise((resolve) => {
-    xhr.onreadystatechange = function () {
-    	if (xhr.readyState === 4) {
-	    	if (xhr.status >= 200 && xhr.status <= 299) {
-	    		resolve(xhr.response);
-	    	}
-	    }
+  return new Promise(function(resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(methodRequest, url, true);
+    xhr.onload = function() {
+      if (this.status >= 200 && this.status <= 299) {
+        resolve(this.response);
+      } else {
+        let error = new Error(this.statusText);
+        error.code = this.status;
+        reject(error);
+      }
     };
+    xhr.onerror = function() {
+      reject(new Error("Произошла ошибка соединения"));
+    };
+    xhr.send(bodyRequest);
   });
 };
